38f1b0cbc14948088f95b5006ba7c9f2baeae12a
Bryan Denny <mandlar@gmail.com>
2025-05-09 16:08:23 -0400

    Added MobileUO comment marker and fixed typo

--- Assets/Scripts/ClassicUO/src/ClassicUO.IO/FileReader.cs
+++ Assets/Scripts/ClassicUO/src/ClassicUO.IO/FileReader.cs
@@ -29,7 +29,8 @@ namespace ClassicUO.IO
         }
 
         public void Seek(long index, SeekOrigin origin) => _position = Reader.BaseStream.Seek(index, origin);
-        public byte[] ReadBytes(int lenght) { _position += lenght; return Reader.ReadBytes(lenght); }
+        // MobileUO: added method
+        public byte[] ReadBytes(int length) { _position += length; return Reader.ReadBytes(length); }
         public sbyte ReadInt8() { _position += sizeof(sbyte); return Reader.ReadSByte(); }
         public byte ReadUInt8() { _position += sizeof(byte); return Reader.ReadByte(); }
         public short ReadInt16() { _position += sizeof(short); return Reader.ReadInt16(); }
