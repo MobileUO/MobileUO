ca14315bd1f5d4a826bad35c8971504b94c97b24
Bryan Denny <mandlar@gmail.com>
2025-09-19 12:54:48 -0400

    Improving mesh holder performance

--- Assets/Scripts/XNAEmulator/Graphics/MeshHolder.cs
+++ Assets/Scripts/XNAEmulator/Graphics/MeshHolder.cs
@@ -1,190 +1,131 @@
 using ClassicUO.Renderer;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityVector2 = UnityEngine.Vector2;
+using UnityVector3 = UnityEngine.Vector3;
 using static ClassicUO.Renderer.UltimaBatcher2D;
 
 namespace Microsoft.Xna.Framework.Graphics
 {
     internal class MeshHolder
     {
+        private struct MeshVertex
+        {
+            public UnityVector3 Position;
+            public UnityVector3 Normal;
+            public UnityVector2 TexCoord;
+        }
+
         public readonly Mesh Mesh;
 
-        //private UnityEngine.Vector3[] vertices;
-        //private UnityEngine.Vector2[] uvs;
-        //private UnityEngine.Vector3[] normals;
-        //private int[] triangles;
-
-        private readonly List<UnityEngine.Vector3> vertices = new List<UnityEngine.Vector3>();
-        private readonly List<UnityEngine.Vector2> uvs = new List<UnityEngine.Vector2>();
-        private readonly List<UnityEngine.Vector3> normals = new List<UnityEngine.Vector3>();
-        private readonly List<int> tris = new List<int>();
+        private MeshVertex[] _vertexBuffer = System.Array.Empty<MeshVertex>();
+        private uint[] _indexBuffer = System.Array.Empty<uint>();
+        private int _quadCapacity;
+        private readonly Bounds _bounds;
+        private readonly VertexAttributeDescriptor[] _vertexLayout;
+        private IndexFormat _indexFormat = IndexFormat.UInt32;
 
         public MeshHolder(int quadCount)
         {
             Mesh = new Mesh();
-            Mesh.bounds = new Bounds(
-              new Vector3(0, 0, 0),
-              new Vector3(20000f, 20000f, 1f)
-            );
             Mesh.MarkDynamic();
 
-            quadCount = Mathf.NextPowerOfTwo(quadCount);
-            int vCount = quadCount * 4;
+            _bounds = new Bounds(new UnityVector3(0f, 0f, 0f), new UnityVector3(20000f, 20000f, 1f));
+            Mesh.bounds = _bounds;
 
-            // optional: pre-size lists
-            int tCount = quadCount * 6;
-            vertices.Capacity = vCount;
-            uvs.Capacity = vCount;
-            normals.Capacity = vCount;
-            tris.Capacity = tCount;
+            _vertexLayout = new[]
+            {
+                new VertexAttributeDescriptor(VertexAttribute.Position, VertexAttributeFormat.Float32, 3),
+                new VertexAttributeDescriptor(VertexAttribute.Normal, VertexAttributeFormat.Float32, 3),
+                new VertexAttributeDescriptor(VertexAttribute.TexCoord0, VertexAttributeFormat.Float32, 2)
+            };
 
-            //vertices = new UnityEngine.Vector3[vCount];
-            //uvs = new UnityEngine.Vector2[vCount];
-            //normals = new UnityEngine.Vector3[vCount];
-
-            //var triangles = new int[quadCount * 6];
-            //for (var i = 0; i < quadCount; i++)
-            //{
-            //    /*
-            //     *  TL    TR
-            //     *   0----1 0,1,2,3 = index offsets for vertex indices
-            //     *   |   /| TL,TR,BL,BR are vertex references in SpriteBatchItem.
-            //     *   |  / |
-            //     *   | /  |
-            //     *   |/   |
-            //     *   2----3
-            //     *  BL    BR
-            //     */
-            //    // Triangle 1
-            //    triangles[i * 6] = i * 4;
-            //    triangles[i * 6 + 1] = i * 4 + 1;
-            //    triangles[i * 6 + 2] = i * 4 + 2;
-            //    // Triangle 2
-            //    triangles[i * 6 + 3] = i * 4 + 1;
-            //    triangles[i * 6 + 4] = i * 4 + 3;
-            //    triangles[i * 6 + 5] = i * 4 + 2;
-            //}
-
-            //Mesh.vertices = vertices;
-            //Mesh.uv = uvs;
-            //Mesh.triangles = triangles;
-            //Mesh.normals = normals;
+            EnsureCapacity(Mathf.Max(1, quadCount));
         }
 
-        //internal void Populate(UltimaBatcher2D.PositionNormalTextureColor4 vertex)
-        //{
-        //    vertex.TextureCoordinate0.y = 1 - vertex.TextureCoordinate0.y;
-        //    vertices[0] = vertex.Position0;
-        //    uvs[0] = vertex.TextureCoordinate0;
-        //    normals[0] = vertex.Normal0;
-
-        //    vertex.TextureCoordinate1.y = 1 - vertex.TextureCoordinate1.y;
-        //    vertices[1] = vertex.Position1;
-        //    uvs[1] = vertex.TextureCoordinate1;
-        //    normals[1] = vertex.Normal1;
-
-        //    vertex.TextureCoordinate2.y = 1 - vertex.TextureCoordinate2.y;
-        //    vertices[2] = vertex.Position2;
-        //    uvs[2] = vertex.TextureCoordinate2;
-        //    normals[2] = vertex.Normal2;
-
-        //    vertex.TextureCoordinate3.y = 1 - vertex.TextureCoordinate3.y;
-        //    vertices[3] = vertex.Position3;
-        //    uvs[3] = vertex.TextureCoordinate3;
-        //    normals[3] = vertex.Normal3;
-
-        //    Mesh.vertices = vertices;
-        //    Mesh.uv = uvs;
-        //    Mesh.normals = normals;
-        //}
-
-        public void Populate(IList<PositionNormalTextureColor4> quads)//, bool isFromTextureAtlas)
+        public void Populate(IList<PositionNormalTextureColor4> quads)
         {
             int quadCount = quads.Count;
-
-            // reuse the same internal buffers
-            vertices.Clear();
-            uvs.Clear();
-            normals.Clear();
-            tris.Clear();
-
-            for (int q = 0; q < quadCount; q++)
+            if (quadCount == 0)
             {
-                int vOff = q * 4;
-                var quad = quads[q];
-
-                // --- positions (snap to nearest pixel)
-                var p0 = quad.Position0; // TL
-                var p1 = quad.Position1; // TR
-                var p2 = quad.Position2; // BL
-                var p3 = quad.Position3; // BR
-
-                p0.x = Mathf.Round(p0.x);
-                p0.y = Mathf.Round(p0.y);
-                p1.x = Mathf.Round(p1.x);
-                p1.y = Mathf.Round(p1.y);
-                p2.x = Mathf.Round(p2.x);
-                p2.y = Mathf.Round(p2.y);
-                p3.x = Mathf.Round(p3.x);
-                p3.y = Mathf.Round(p3.y);
-
-                vertices.Add(p0);
-                vertices.Add(p1);
-                vertices.Add(p2);
-                vertices.Add(p3);
-
-                // --- UVs (flip Y)
-                //if (isFromTextureAtlas)
-                //{
-                //    uvs.Add(quad.TextureCoordinate2); // old BL -> new TL
-                //    uvs.Add(quad.TextureCoordinate3); // old BR -> new TR
-                //    uvs.Add(quad.TextureCoordinate0); // old TL -> new BL
-                //    uvs.Add(quad.TextureCoordinate1); // old TR -> new BR
-                //}
-                //else
-                //{
-                //    var uv0 = quad.TextureCoordinate0; uv0.y = 1f - uv0.y;
-                //    var uv1 = quad.TextureCoordinate1; uv1.y = 1f - uv1.y;
-                //    var uv2 = quad.TextureCoordinate2; uv2.y = 1f - uv2.y;
-                //    var uv3 = quad.TextureCoordinate3; uv3.y = 1f - uv3.y;
-                //    uvs.Add(uv0);
-                //    uvs.Add(uv1);
-                //    uvs.Add(uv2);
-                //    uvs.Add(uv3);
-                //}
-
-                var uv0 = quad.TextureCoordinate0;
-                var uv1 = quad.TextureCoordinate1;
-                var uv2 = quad.TextureCoordinate2;
-                var uv3 = quad.TextureCoordinate3;
-                uvs.Add(uv0);
-                uvs.Add(uv1);
-                uvs.Add(uv2);
-                uvs.Add(uv3);
-
-                // --- normals
-                normals.Add(quad.Normal0);
-                normals.Add(quad.Normal1);
-                normals.Add(quad.Normal2);
-                normals.Add(quad.Normal3);
-
-                // --- triangles (XNA winding)
-                tris.Add(vOff + 0); // TL
-                tris.Add(vOff + 1); // TR
-                tris.Add(vOff + 2); // BL
-
-                tris.Add(vOff + 1); // TR
-                tris.Add(vOff + 3); // BR
-                tris.Add(vOff + 2); // BL
+                Mesh.subMeshCount = 0;
+                return;
             }
 
-            Mesh.Clear();
-            Mesh.SetVertices(vertices);
-            Mesh.SetUVs(0, uvs);
-            Mesh.SetNormals(normals);
-            Mesh.SetTriangles(tris, 0);
-            Mesh.RecalculateBounds();
+            EnsureCapacity(quadCount);
+
+            int vertexCount = quadCount * 4;
+            int indexCount = quadCount * 6;
+
+            for (int q = 0, v = 0; q < quadCount; q++, v += 4)
+            {
+                var quad = quads[q];
+
+                WriteVertex(ref _vertexBuffer[v + 0], quad.Position0, quad.Normal0, quad.TextureCoordinate0);
+                WriteVertex(ref _vertexBuffer[v + 1], quad.Position1, quad.Normal1, quad.TextureCoordinate1);
+                WriteVertex(ref _vertexBuffer[v + 2], quad.Position2, quad.Normal2, quad.TextureCoordinate2);
+                WriteVertex(ref _vertexBuffer[v + 3], quad.Position3, quad.Normal3, quad.TextureCoordinate3);
+            }
+
+            Mesh.SetVertexBufferData(_vertexBuffer, 0, 0, vertexCount, 0,
+                MeshUpdateFlags.DontValidateIndices | MeshUpdateFlags.DontRecalculateBounds);
+
+            Mesh.SetIndexBufferData(_indexBuffer, 0, 0, indexCount,
+                MeshUpdateFlags.DontValidateIndices | MeshUpdateFlags.DontRecalculateBounds);
+
+            var subMesh = new SubMeshDescriptor(0, indexCount, MeshTopology.Triangles)
+            {
+                vertexCount = vertexCount
+            };
+
+            Mesh.subMeshCount = 1;
+            Mesh.SetSubMesh(0, subMesh, MeshUpdateFlags.DontRecalculateBounds);
+            Mesh.bounds = _bounds;
+        }
+
+        private void EnsureCapacity(int quadCount)
+        {
+            quadCount = Mathf.NextPowerOfTwo(Mathf.Max(1, quadCount));
+
+            if (quadCount <= _quadCapacity)
+            {
+                return;
+            }
+
+            _quadCapacity = quadCount;
+
+            int vertexCapacity = quadCount * 4;
+            int indexCapacity = quadCount * 6;
+
+            _vertexBuffer = new MeshVertex[vertexCapacity];
+            _indexBuffer = new uint[indexCapacity];
+
+            for (int q = 0, v = 0, i = 0; q < quadCount; q++, v += 4, i += 6)
+            {
+                _indexBuffer[i + 0] = (uint)(v + 0);
+                _indexBuffer[i + 1] = (uint)(v + 1);
+                _indexBuffer[i + 2] = (uint)(v + 2);
+                _indexBuffer[i + 3] = (uint)(v + 1);
+                _indexBuffer[i + 4] = (uint)(v + 3);
+                _indexBuffer[i + 5] = (uint)(v + 2);
+            }
+
+            _indexFormat = IndexFormat.UInt32;
+
+            Mesh.SetVertexBufferParams(vertexCapacity, _vertexLayout);
+            Mesh.SetIndexBufferParams(indexCapacity, _indexFormat);
+            Mesh.subMeshCount = 1;
+        }
+
+        private static void WriteVertex(ref MeshVertex dst, UnityVector3 position, UnityVector3 normal, UnityVector3 texCoord)
+        {
+            dst.Position.x = Mathf.Round(position.x);
+            dst.Position.y = Mathf.Round(position.y);
+            dst.Position.z = position.z;
+            dst.Normal = normal;
+            dst.TexCoord.x = texCoord.x;
+            dst.TexCoord.y = texCoord.y;
         }
     }
 }
