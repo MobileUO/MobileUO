3022e5699ebddf0361156584d069876619946d40
Bryan Denny <mandlar@gmail.com>
2025-10-30 17:56:49 -0400

    ModernUIConstants dependents

--- Assets/Scripts/ClassicUO/src/ClassicUO.Client/Game/UI/Gumps/DurabilityGump.cs
+++ Assets/Scripts/ClassicUO/src/ClassicUO.Client/Game/UI/Gumps/DurabilityGump.cs
@@ -42,191 +42,189 @@ namespace ClassicUO.Game.UI.Gumps
             return base.Draw(batcher, x, y);
         }
 
-        // MobileUO: TODO: TazUO revisit
-        //protected override void OnMouseUp(int x, int y, MouseButtonType button)
-        //{
-        //    if (button == MouseButtonType.Left && DurabilityManager.HasDurabilityData)
-        //    {
-        //        UIManager.GetGump<DurabilitysGump>()?.Dispose();
-        //        UIManager.Add(new DurabilitysGump(World));
-        //    }
-        //}
+        protected override void OnMouseUp(int x, int y, MouseButtonType button)
+        {
+            if (button == MouseButtonType.Left && DurabilityManager.HasDurabilityData)
+            {
+                UIManager.GetGump<DurabilitysGump>()?.Dispose();
+                UIManager.Add(new DurabilitysGump(World));
+            }
+        }
     }
 
-    // MobileUO: TODO: TazUO revisit
-    //internal class DurabilitysGump : NineSliceGump
-    //{
-    //    private static int lastWidth = 300, lastHeight = 400;
-    //    private static int lastX, lastY;
+    internal class DurabilitysGump : NineSliceGump
+    {
+        private static int lastWidth = 300, lastHeight = 400;
+        private static int lastX, lastY;
 
-    //    private enum DurabilityColors
-    //    {
-    //        RED = 0x0805,
-    //        BLUE = 0x0806,
-    //        GREEN = 0x0808,
-    //        YELLOW = 0x0809
-    //    }
+        private enum DurabilityColors
+        {
+            RED = 0x0805,
+            BLUE = 0x0806,
+            GREEN = 0x0808,
+            YELLOW = 0x0809
+        }
 
-    //    private readonly Dictionary<string, ContextMenuItemEntry> _menuItems = new Dictionary<string, ContextMenuItemEntry>();
-    //    private VBoxContainer _dataBox;
-    //    public override GumpType GumpType => GumpType.DurabilityGump;
+        private readonly Dictionary<string, ContextMenuItemEntry> _menuItems = new Dictionary<string, ContextMenuItemEntry>();
+        private VBoxContainer _dataBox;
+        public override GumpType GumpType => GumpType.DurabilityGump;
 
-    //    public DurabilitysGump(World world) : base(world, lastX, lastY, lastWidth, lastHeight, ModernUIConstants.ModernUIPanel, ModernUIConstants.ModernUIPanel_BoderSize, true, 200, 200)
-    //    {
-    //        LayerOrder = UILayer.Default;
-    //        CanCloseWithRightClick = true;
-    //        CanMove = true;
+        public DurabilitysGump(World world) : base(world, lastX, lastY, lastWidth, lastHeight, ModernUIConstants.ModernUIPanel, ModernUIConstants.ModernUIPanel_BoderSize, true, 200, 200)
+        {
+            LayerOrder = UILayer.Default;
+            CanCloseWithRightClick = true;
+            CanMove = true;
 
-    //        Width = lastWidth;
-    //        Height = lastHeight;
+            Width = lastWidth;
+            Height = lastHeight;
 
-    //        X = lastX;
-    //        Y = lastY;
+            X = lastX;
+            Y = lastY;
 
-    //        if (lastX == 0 || lastY == 0)
-    //        {
-    //            X = lastX = (Client.Game.Scene.Camera.Bounds.Width - Width) / 2;
-    //            Y = lastY = Client.Game.Scene.Camera.Bounds.Y + 20;
-    //        }
+            if (lastX == 0 || lastY == 0)
+            {
+                X = lastX = (Client.Game.Scene.Camera.Bounds.Width - Width) / 2;
+                Y = lastY = Client.Game.Scene.Camera.Bounds.Y + 20;
+            }
 
-    //        Build();
-    //    }
+            Build();
+        }
 
-    //    private void Build()
-    //    {
-    //        Clear();
+        private void Build()
+        {
+            Clear();
 
-    //        BuildHeader();
+            BuildHeader();
 
-    //        ScrollArea area = new ScrollArea(10, 30, Width - 20, Height - 50, true)
-    //        {
-    //            ScrollbarBehaviour = ScrollbarBehaviour.ShowAlways
-    //        };
+            ScrollArea area = new ScrollArea(10, 30, Width - 20, Height - 50, true)
+            {
+                ScrollbarBehaviour = ScrollbarBehaviour.ShowAlways
+            };
 
-    //        Add(area);
+            Add(area);
 
-    //        _dataBox = new VBoxContainer(Width - 40);
-    //        area.Add(_dataBox);
+            _dataBox = new VBoxContainer(Width - 40);
+            area.Add(_dataBox);
 
-    //        RequestUpdateContents();
-    //    }
+            RequestUpdateContents();
+        }
 
 
-    //    private void BuildHeader()
-    //    {
-    //        Label l = new("Equipment Durability", true, 0xFF);
-    //        l.X = (Width >> 1) - (l.Width >> 1);
-    //        l.Y = (l.Height >> 1) >> 1;
+        private void BuildHeader()
+        {
+            Label l = new("Equipment Durability", true, 0xFF);
+            l.X = (Width >> 1) - (l.Width >> 1);
+            l.Y = (l.Height >> 1) >> 1;
 
-    //        Add(l);
-    //    }
+            Add(l);
+        }
 
-    //    public override void Dispose()
-    //    {
-    //        base.Dispose();
-    //        lastX = X;
-    //        lastY = Y;
-    //    }
+        public override void Dispose()
+        {
+            base.Dispose();
+            lastX = X;
+            lastY = Y;
+        }
 
-    //    protected override void UpdateContents()
-    //    {
-    //        _dataBox.Clear();
-    //        Rectangle barBounds = Client.Game.UO.Gumps.GetGump((uint)DurabilityColors.RED).UV;
+        protected override void UpdateContents()
+        {
+            _dataBox.Clear();
+            Rectangle barBounds = Client.Game.UO.Gumps.GetGump((uint)DurabilityColors.RED).UV;
 
-    //        var items = World.DurabilityManager?.Durabilities ?? new List<DurabiltyProp>();
+            var items = World.DurabilityManager?.Durabilities ?? new List<DurabiltyProp>();
 
-    //        foreach (var durability in items.OrderBy(d => d.Percentage))
-    //        {
-    //            if (durability.MaxDurabilty <= 0)
-    //            {
-    //                continue;
-    //            }
+            foreach (var durability in items.OrderBy(d => d.Percentage))
+            {
+                if (durability.MaxDurabilty <= 0)
+                {
+                    continue;
+                }
 
-    //            var item = World.Items.Get((uint)durability.Serial);
+                var item = World.Items.Get((uint)durability.Serial);
 
-    //            if (item == null)
-    //            {
-    //                continue;
-    //            }
+                if (item == null)
+                {
+                    continue;
+                }
 
-    //            var a = new Area();
-    //            a.AcceptMouseInput = true;
-    //            a.WantUpdateSize = false;
-    //            a.CanMove = true;
-    //            a.Height = 44;
-    //            a.Width = Width - (a.X * 2) - 40;
+                var a = new Area();
+                a.AcceptMouseInput = true;
+                a.WantUpdateSize = false;
+                a.CanMove = true;
+                a.Height = 44;
+                a.Width = Width - (a.X * 2) - 40;
 
-    //            Label name;
-    //            a.Add(name = new Label($"{(string.IsNullOrWhiteSpace(item.Name) ? item.Layer : item.Name)}", true, 0xFFFF, ishtml: true));
-    //            GumpPic red;
-    //            a.Add(red = new GumpPic(0, name.Y + name.Height + 5, (ushort)DurabilityColors.RED, 0));
+                Label name;
+                a.Add(name = new Label($"{(string.IsNullOrWhiteSpace(item.Name) ? item.Layer : item.Name)}", true, 0xFFFF, ishtml: true));
+                GumpPic red;
+                a.Add(red = new GumpPic(0, name.Y + name.Height + 5, (ushort)DurabilityColors.RED, 0));
 
-    //            DurabilityColors statusGump = DurabilityColors.GREEN;
+                DurabilityColors statusGump = DurabilityColors.GREEN;
 
-    //            if (durability.Percentage < 0.7)
-    //            {
-    //                statusGump = DurabilityColors.YELLOW;
-    //            }
-    //            else if (durability.Percentage < 0.95)
-    //            {
-    //                statusGump = DurabilityColors.BLUE;
-    //            }
+                if (durability.Percentage < 0.7)
+                {
+                    statusGump = DurabilityColors.YELLOW;
+                }
+                else if (durability.Percentage < 0.95)
+                {
+                    statusGump = DurabilityColors.BLUE;
+                }
 
-    //            if (durability.Percentage > 0)
-    //            {
-    //                a.Add(new GumpPicTiled(0, red.Y, (int)Math.Floor(barBounds.Width * durability.Percentage), barBounds.Height, (ushort)statusGump));
-    //            }
+                if (durability.Percentage > 0)
+                {
+                    a.Add(new GumpPicTiled(0, red.Y, (int)Math.Floor(barBounds.Width * durability.Percentage), barBounds.Height, (ushort)statusGump));
+                }
 
-    //            var durWidth = Client.Game.UO.FileManager.Fonts.GetWidthUnicode(0, $"{durability.Durabilty} / {durability.MaxDurabilty}");
+                var durWidth = Client.Game.UO.FileManager.Fonts.GetWidthUnicode(0, $"{durability.Durabilty} / {durability.MaxDurabilty}");
 
-    //            a.Add
-    //            (
-    //                new Label($"{durability.Durabilty} / {durability.MaxDurabilty}", true, 0xFFFF)
-    //                {
-    //                    Y = red.Y - 2,
-    //                    X = Width - 38 - durWidth
-    //                }
-    //            );
+                a.Add
+                (
+                    new Label($"{durability.Durabilty} / {durability.MaxDurabilty}", true, 0xFFFF)
+                    {
+                        Y = red.Y - 2,
+                        X = Width - 38 - durWidth
+                    }
+                );
 
-    //            a.MouseUp += (s, e) =>
-    //            {
-    //                if (e.Button == MouseButtonType.Left && World.TargetManager.IsTargeting)
-    //                {
-    //                    World.TargetManager.Target(item);
-    //                }
-    //            };
+                a.MouseUp += (s, e) =>
+                {
+                    if (e.Button == MouseButtonType.Left && World.TargetManager.IsTargeting)
+                    {
+                        World.TargetManager.Target(item);
+                    }
+                };
 
-    //            _dataBox.Add(a);
-    //        }
-    //    }
+                _dataBox.Add(a);
+            }
+        }
 
-    //    protected override void OnResize(int oldWidth, int oldHeight, int newWidth, int newHeight)
-    //    {
-    //        base.OnResize(oldWidth, oldHeight, newWidth, newHeight);
-    //        Build();
-    //        lastWidth = newWidth;
-    //        lastHeight = newHeight;
-    //    }
+        protected override void OnResize(int oldWidth, int oldHeight, int newWidth, int newHeight)
+        {
+            base.OnResize(oldWidth, oldHeight, newWidth, newHeight);
+            Build();
+            lastWidth = newWidth;
+            lastHeight = newHeight;
+        }
 
-    //    public override void Save(XmlTextWriter writer)
-    //    {
-    //        base.Save(writer);
+        public override void Save(XmlTextWriter writer)
+        {
+            base.Save(writer);
 
-    //        writer.WriteAttributeString("lastX", X.ToString());
-    //        writer.WriteAttributeString("lastY", Y.ToString());
-    //        writer.WriteAttributeString("lastWidth", lastWidth.ToString());
-    //        writer.WriteAttributeString("lastHeight", lastHeight.ToString());
-    //    }
+            writer.WriteAttributeString("lastX", X.ToString());
+            writer.WriteAttributeString("lastY", Y.ToString());
+            writer.WriteAttributeString("lastWidth", lastWidth.ToString());
+            writer.WriteAttributeString("lastHeight", lastHeight.ToString());
+        }
 
-    //    public override void Restore(XmlElement xml)
-    //    {
-    //        base.Restore(xml);
+        public override void Restore(XmlElement xml)
+        {
+            base.Restore(xml);
 
-    //        int.TryParse(xml.GetAttribute("lastX"), out X);
-    //        int.TryParse(xml.GetAttribute("lastY"), out Y);
-    //        int.TryParse(xml.GetAttribute("lastWidth"), out Width);
-    //        int.TryParse(xml.GetAttribute("lastHeight"), out Height);
-    //        Build();
-    //    }
-    //}
+            int.TryParse(xml.GetAttribute("lastX"), out X);
+            int.TryParse(xml.GetAttribute("lastY"), out Y);
+            int.TryParse(xml.GetAttribute("lastWidth"), out Width);
+            int.TryParse(xml.GetAttribute("lastHeight"), out Height);
+            Build();
+        }
+    }
 }
