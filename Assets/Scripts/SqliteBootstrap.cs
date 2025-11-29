b4154cc5fd80e9139f11fc0a002d405ac1de2e30
Bryan Denny <mandlar@gmail.com>
2025-11-29 00:41:45 -0500

    Manually added Android .so for sqlite

--- Assets/Scripts/SqliteBootstrap.cs
+++ Assets/Scripts/SqliteBootstrap.cs
@@ -7,7 +7,10 @@ public static class SqliteBootstrap
     private static void Init()
     {
         // Wire up SQLitePCLRaw for Android/iOS
-        Batteries_V2.Init();
+        // Force SQLitePCLRaw to use the e_sqlite3 provider
+        raw.SetProvider(new SQLite3Provider_e_sqlite3());
+        //Batteries_V2.Init();
+
         Debug.Log("[SqliteBootstrap] SQLitePCLRaw initialized");
     }
 }
