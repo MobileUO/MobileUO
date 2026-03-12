c4f270e209e85b643bd476d14187dafc320991f1
Sascha <k.puligheddu@alice.it>
2026-03-12 20:38:26 +0100

    Added AGPLv3 licensing for Assistant files (#66)

--- Assets/Scripts/Assistant/UOMobile.cs
+++ Assets/Scripts/Assistant/UOMobile.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿#region license
+// Copyright (C) 2022-2025 Sascha Puligheddu
+// 
+// This project is a complete reproduction of AssistUO for MobileUO and ClassicUO.
+// Developed as a lightweight, native assistant.
+// 
+// Licensed under the GNU Affero General Public License v3.0 (AGPL-3.0).
+// 
+// SPECIAL PERMISSION: Integration with projects under BSD 2-Clause (like ClassicUO)
+// is permitted, provided that the integrated result remains publicly accessible 
+// and the AGPL-3.0 terms are respected for this specific module.
+//
+// This program is distributed WITHOUT ANY WARRANTY. 
+// See <https://www.gnu.org> for details.
+#endregion
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Text;
